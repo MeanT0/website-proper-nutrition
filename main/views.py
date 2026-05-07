756b2afc5692c214f759f697be58dcdbe4ef08fd
MeanT0 <meantoon0@gmail.com>
2026-05-07 21:38:32 +0300

    Fix: define data variable in views.py

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'main/index.html', data)
+    return render(request, 'main/index.html')
 
 
 def about(request):
